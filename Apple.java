016c74aac25824a2c48008d5f8ff97d9e678f4a9
Dipali Ubhare <dipaliubhare@gmail.com>
2023-01-25 17:18:37 +0530

    Mango and Apple

---
+++ Apple.java
@@ -0,0 +1,8 @@
+package Fruit;
+
+public class Apple {
+	public static void main(String[] args) {
+		System.out.println("welcome apple");
+	}
+
+}
---
+++ Mango.java
@@ -0,0 +1,10 @@
+package Fruit;
+
+public class Mango {
+
+	public static void main(String[] args) {
+		System.out.println("welcome mango");
+
+	}
+
+}
